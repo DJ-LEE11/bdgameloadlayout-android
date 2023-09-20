1a742fe3df5b15cff5cfe93f74f27bf9c4d94df4
lidongjie2 <lidongjie2@yy.com>
2023-09-20 18:32:27 +0800

    txt文件解析

--- app/src/main/java/com/bdgame/loadlayout/layout/LayoutManager.kt
+++ app/src/main/java/com/bdgame/loadlayout/layout/LayoutManager.kt
@@ -30,30 +30,29 @@ object LayoutManager {
     fun updateLayout() {
         KLog.i(TAG, "updateLayout start")
         mScope.launch {
-            val layoutFile = File("${AppRuntime.application.filesDir.path}/layout/layout.json")
+            val layoutFile = File("${AppRuntime.application.filesDir.path}/layout/layout.txt")
             if (layoutFile.exists()) {
                 KLog.i(TAG, "updateLayout layoutFile exists")
-                convertJsonFileToString(layoutFile).apply {
+                convertTextFileToString(layoutFile).apply {
                     KLog.i(TAG, "updateLayout json content:$this")
                 }
             }
         }
     }
 
-    private fun convertJsonFileToString(file: File): String {
+    private fun convertTextFileToString(file: File): String {
         val stringBuilder = StringBuilder()
-
         try {
             val bufferedReader = BufferedReader(FileReader(file))
             var line: String?
             while (bufferedReader.readLine().also { line = it } != null) {
                 stringBuilder.append(line)
+                stringBuilder.append("\n") // 如果需要保留换行符，可以注释掉这一行
             }
             bufferedReader.close()
         } catch (e: IOException) {
             e.printStackTrace()
         }
-
         return stringBuilder.toString()
     }
 }
